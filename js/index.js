fbac30e70ee85d719c8f371274edc914bb7cddef
Christelle <adeolamelissasissoko95@gmail.com>
2017-09-11 16:17:59 -0400

    Latests changes

--- js/index.js
+++ js/index.js
@@ -1,34 +1,5 @@
 
-/* Source : http://callmenick.com/_development/image-overlay-hover-effects/ */
 $(document).ready(function(){
-    // if (Modernizr.touch) {
-    //     // show the close overlay button
-    //     $(".close-overlay").removeClass("hidden");
-    //     // handle the adding of hover class when clicked
-    //     $(".img").click(function(e){
-    //         if (!$(this).hasClass("hover")) {
-    //             $(this).addClass("hover");
-    //         }
-    //     });
-    //     // handle the closing of the overlay
-    //     $(".close-overlay").click(function(e){
-    //         e.preventDefault();
-    //         e.stopPropagation();
-    //         if ($(this).closest(".img").hasClass("hover")) {
-    //             $(this).closest(".img").removeClass("hover");
-    //         }
-    //     });
-    // } else {
-    //     // handle the mouseenter functionality
-    //     $(".img").mouseenter(function(){
-    //         $(this).addClass("hover");
-    //     })
-    //     // handle the mouseleave functionality
-    //     .mouseleave(function(){
-    //         $(this).removeClass("hover");
-    //     });
-    // }
-
     $("#chevron_down").click(function() {
         $('html,body').animate({
             scrollTop: $("section#prototype").offset().top + (-75)},
@@ -59,14 +30,6 @@ $(document).ready(function(){
             scrollTop: $("section#quote_commanditaires").offset().top + (-80)},
             1000);
     });
-
-
-
-
-
-
-
-
 
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
